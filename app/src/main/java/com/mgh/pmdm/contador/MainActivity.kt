95e39e8582c3701bbb3c76f13d25bc1cfe528ce8
angelrodriguezdam@hotmail.com <angelrodriguezdam@hotmail.com>
2023-10-23 10:01:18 +0200

    añadido

--- app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
+++ app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.mgh.pmdm.contador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,29 +13,45 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val miDialogo = MiDialogFragment()
+
 
         // Referencia al TextView
-        val textViewContador=findViewById<TextView>(R.id.texto)
+        //val textViewContador=findViewById<TextView>(R.id.texto)
 
         // Inicializamos el TextView con el contador a 0
-        textViewContador.setText(contador.toString())
+        //textViewContador.setText(contador.toString())
 
         // Referencia al botón
-        val btAdd=findViewById<Button>(R.id.btAdd)
-        val botonMenos=findViewById<Button>(R.id.botonMenos)
+        //val btAdd=findViewById<Button>(R.id.btAdd)
+        //val botonMenos=findViewById<Button>(R.id.botonMenos)
 
         // Asociaciamos una expresióin lambda como
         // respuesta (callback) al evento Clic sobre
         // el botón
-        btAdd.setOnClickListener {
+        binding.btAdd.setOnClickListener {
+
             contador++
-            textViewContador.setText(contador.toString())
+
+            binding.texto.setText(contador.toString())
         }
 
-        botonMenos.setOnClickListener{
+        binding.botonMenos.setOnClickListener {
+
             contador--
-            textViewContador.setText(contador.toString())
+
+            binding.texto.setText(contador.toString())
+        }
+
+        binding.buttonReset.setOnClickListener {
+            contador = 0
+            binding.texto.setText(contador.toString())
+            miDialogo.show(supportFragmentManager, "Mi dialogo")
         }
 
     }
